7ea732496a3b28868737b6ca8be7fc5be0d5d97d
Fiterman <gfiterman96@gmail.com>
2023-11-16 04:08:28 -0300

    feat: create gallery service and returns jsonplaceholder api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -10,7 +11,9 @@ import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { MatIconModule } from '@angular/material/icon';
+
 import { GalleryComponent } from './components/gallery/gallery.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -26,7 +29,8 @@ import { GalleryComponent } from './components/gallery/gallery.component';
     MatCardModule,
     MatInputModule,
     ReactiveFormsModule,
-    MatIconModule
+    MatIconModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { GalleryService } from '../../services/gallery.service';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.scss'],
 })
 export class GalleryComponent implements OnInit {
+  albums: any[] = [];
 
-  constructor() { }
+  constructor(private galleryService: GalleryService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getAlbums();
   }
 
+  getAlbums() {
+    this.galleryService.getAlbums().subscribe(
+      (data) => {
+        this.albums = data;
+        console.log('Álbuns:', this.albums);
+      },
+      (error) => {
+        console.error('Erro ao obter álbuns:', error);
+      }
+    );
+  }
 }
